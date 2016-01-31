d52ccef51540bcb9bc97c98ee63f741dc44e72e4
Ilja Livenson <ilja@opennodecloud.com>
2016-01-31 08:35:36 +0200

    Fix tab/spaces usage in Main and Router

--- src/fox/Main.go
+++ src/fox/Main.go
@@ -11,7 +11,7 @@ func main()  {
 	nodeName = os.Args[1]
 
 	router := NewRouter()
-    var port int = 8090
-    log.Printf("Starting a server on localhost:%d", port)
+	var port int = 8090
+	log.Printf("Starting a server on localhost:%d", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
 }
