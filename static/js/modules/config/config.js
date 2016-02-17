19d3540ae77f277ca5ce2f06784a866939ffe474
Mihkel Kasepuu <mihkel.kasepuu@gmail.com>
2016-02-17 21:38:42 +0200

    UI get and add func

--- static/js/modules/config/config.js
+++ static/js/modules/config/config.js
@@ -5,25 +5,14 @@
 
 var foxApp = angular.module("fox", []);
 
+foxApp.config(function ( $httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    //$httpProvider.defaults.withCredentials = true;
+    delete $httpProvider.defaults.headers.common["X-Requested-With"];
+    $httpProvider.defaults.headers.common["Accept"] = "application/json";
+    $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+});
 
-foxApp.controller('RegisterController', ['$scope', function ($scope) {
-    $scope.testing = 'Hola!';
-
-    $scope.foxList = [{id: 1, name: "Reinuvader"}, {id: 2, name: "Juhan"}];
-
-    //TODO LISA FOX ADD IMPL
-    $scope.add = function (fox) {
-        console.log(fox);
-    };
-
-    //TODO LISA FOX UPDATE IMPL
-    $scope.update = function(fox) {
-        console.log(fox);
-    };
-
-    //TODO LISA FOX DELETE IMPL
-    $scope.delete = function(fox) {
-        console.log(fox);
-    };
-
-}]);
+foxApp.constant("configConstant", {
+   backendURL: 'http://localhost:8090'
+});
