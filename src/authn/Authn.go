6dcc27ac61bfdfdbf4f4f9793f6fe5fd018f19b6
Andres Kütt <andres.kytt@gmail.com>
2016-02-27 21:04:55 +0200

    Key loading is now threadsafe, added token validation functionality

--- src/authn/Authn.go
+++ src/authn/Authn.go
@@ -1,5 +1,10 @@
 package authn
 
+import(
+	"errors"
+)
+
+
 // Authenticate sends the username and challenge to the authentication 
 // provider requested and passes the resulting boolean back.
 // Any non-true result including technical failures and the authentication 
@@ -11,5 +16,10 @@ func Authenticate(username string, challenge string, provider string) bool  {
 
 // Validate the Fernet token and extract the username
 func Validate(token string) (string, error){
-	return "notimplemented", nil
+	
+	t := Decrypt(token)
+	if t == nil {
+		return "", errors.New("Invalid token")
+	}
+	return t.Username, nil
 }
--- src/authn/Token.go
+++ src/authn/Token.go
@@ -1,61 +1,97 @@
 package authn
 
-import(
-	"io/ioutil"
-	fernet "github.com/fernet/fernet-go"
+import (
 	"encoding/json"
+	fernet "github.com/fernet/fernet-go"
+	"io/ioutil"
+	"sync"
 	"time"
 )
 
-// TokenStruct contains the payload structure minted into the auth tokens 
+// TokenStruct contains the payload structure minted into the auth tokens
 type TokenStruct struct {
-	Username	string	`json:"username"`
-	MintTime 	string  `json:"mintTime"`
+	Username string `json:"username"`
+	MintTime string `json:"mintTime"`
 }
 
 // Token is the return type for the API
 type Token struct {
-	Token 		string `json:"token"`
+	Token string `json:"token"`
 }
 
 // Global variable to keep the key in
 var mintKey *fernet.Key
 var keyName = "key.base64"
+var tokenTTL = 15 // Time To Live of the tokens in minutes
+
+var lock = new(sync.RWMutex)
 
 // LoadKey initializes a Fernet key based on the contents of the fixed filename.
 // It panics, if the file cannot be found
-func LoadKey(){
+func LoadKey() {
 	LoadKeyByName(keyName)
 }
 
-func LoadKeyByName(filename string)  {
+// LoadKeyByName loads a key by filename. Used for testing
+// In normal operation, LoadKey should be called
+func LoadKeyByName(filename string) {
 	b, err := ioutil.ReadFile(filename)
-	
-	if err != nil{
-		panic(err)		
+
+	if err != nil {
+		panic(err)
 	}
 
 	k, err := fernet.DecodeKey(string(b))
-	
-	if err != nil{
-		panic(err)		
+
+	if err != nil {
+		panic(err)
 	}
 
 	// Store only after we are sure loading was good
-	mintKey = k	
+	lock.Lock()
+	defer lock.Unlock()
+	mintKey = k
 }
 
 // GetToken wraps the incoming username into a TokenStruct, serializes the result to json
 // and generates a Fernet token based on the resulting string
-func GetToken(username string) string{
+func GetToken(username string) string {
 	var m []byte
-	
+
 	t, _ := time.Now().MarshalText()
 	m, _ = json.Marshal(TokenStruct{Username: username, MintTime: string(t)})
-	
+
 	token, err := fernet.EncryptAndSign(m, mintKey)
-	if err != nil{
+	if err != nil {
 		panic(err)
-	}		
+	}
 	return string(token)
+}
+
+// Decrypt decrypts a string containing a token
+// It returns nil if 
+// - the token has been minted more than tokenTTL minutes ago
+// - the token message is not a valid TokenStruct
+// - the token cannot be decrypted using known keys
+func Decrypt(token string) *TokenStruct{
+	var message TokenStruct
+	
+	tok := []byte(token)
+	k := make([]*fernet.Key, 1)
+	m := fernet.VerifyAndDecrypt(tok, time.Duration(tokenTTL)*time.Minute, append(k, GetKey())) 
+
+	err := json.Unmarshal(m, message)
+	if err != nil {
+		return nil
+	}
+	return &message
+}
+
+// GetKey returns the current key used for session tokens
+// If the key not initialized, nil is returned
+func GetKey() *fernet.Key {
+	lock.RLock()
+	defer lock.RUnlock()
+	return mintKey
+
 }
