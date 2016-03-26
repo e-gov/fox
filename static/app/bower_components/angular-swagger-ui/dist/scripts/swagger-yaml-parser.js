5d61379c4fb0dc3ff3ce7ba626fd7b7780332fb3
Mihkel Kasepuu <mihkel.kasepuu.001@gmail.com>
2016-03-26 21:32:08 +0200

    added swagger impl in angular

---
+++ static/app/bower_components/angular-swagger-ui/dist/scripts/swagger-yaml-parser.js
@@ -0,0 +1,51 @@
+/*
+ * Orange angular-swagger-ui - v0.3.1
+ *
+ * (C) 2015 Orange, all right reserved
+ * MIT Licensed
+ */
+'use strict';
+
+angular
+    .module('swaggerUi')
+    .service('swaggerUiYamlParser', function ($window, $q, swaggerParser, swaggerTranslator) {
+
+        /**
+         * Module entry point
+         */
+        this.execute = function (parserType, url, contentType, data, isTrustedSources, parseResult) {
+            var deferred = $q.defer();
+            if (parserType === 'yaml' || (parserType === 'auto' && (contentType === 'application/yaml' || contentType === 'text/yaml'))) {
+                var YAML = $window.jsyaml;
+                if (typeof YAML === 'undefined') {
+                    deferred.reject({
+                        code: 500,
+                        message: swaggerTranslator.translate('errorNoYamlParser')
+                    });
+                } else {
+                    try {
+                        var json = YAML.load(data);
+                        swaggerParser
+                            .execute('json', url, 'application/json', json, isTrustedSources, parseResult)
+                            .then(function (executed) {
+                                parseResult.transformSwagger = json;
+                                deferred.resolve(executed);
+                            })
+                            .catch(deferred.reject);
+                    } catch (e) {
+                        deferred.reject({
+                            code: 500,
+                            message: swaggerTranslator.translate('errorParseFailed', e)
+                        });
+                    }
+                }
+            } else {
+                deferred.resolve(false);
+            }
+            return deferred.promise;
+        };
+
+    })
+    .run(function (swaggerModules, swaggerUiYamlParser) {
+        swaggerModules.add(swaggerModules.PARSE, swaggerUiYamlParser);
+    });
--- static/app/modules/config/config.js
+++ static/app/modules/config/config.js
@@ -1,13 +1,31 @@
 /**
  * Created by mihkelk on 16.02.2016.
  */
-var foxApp = angular.module("fox", []);
+var foxApp = angular.module("fox", [
+    'ngRoute',
+    'ngSanitize',
+    'swaggerUi'
+]);
 
-foxApp.config(function ( $httpProvider) {
+foxApp.config(function ($httpProvider) {
     $httpProvider.defaults.headers.common["Accept"] = "application/json";
     $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 });
 
 foxApp.constant("configConstant", {
-   backendURL: 'http://localhost:9000/api'
+    backendURL: 'http://localhost:9000/api'
+});
+
+foxApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'RegisterController'
+        })
+        .when('/swagger', {
+            templateUrl: 'views/swagger.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 });
