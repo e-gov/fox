42d0ff28dd8fc84a91f255c684ec846d0e012e72
Peeter Kokk <peeter.kokk@gmail.com>
2016-06-23 20:02:41 +0300

    fix #65

--- bash_scripts/build_backend.sh
+++ bash_scripts/build_backend.sh
@@ -9,8 +9,8 @@ go get -d -t fox/fox_test
 go get -d -t login/login_test
 
 mkdir -p config/$USER
-cp -n src/config/config.json.template config/$USER/config.json
-cp -n src/config/config.json.template config/$USER/test_config.json
+cp -n config/config.json.template config/$USER/config.json
+cp -n config/config.json.template config/$USER/test_config.json
 
 mkdir /tmp/foxdb  
 ./bin/foxservice
