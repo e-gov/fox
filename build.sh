7f4ae140549e52e859b5e14c9f8f79a6b549f465
Peeter Kokk <peeter.kokk@gmail.com>
2016-07-10 18:42:34 +0300

    fix build, so npm gets installed

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ fi
 if [ $(dpkg-query -W -f='${Status}' npm 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     read -p "npm required but not installed. Install? [Y/n]: " answer
     answer=${answer:=Y}
-    [[ $answer =~ [Yy] ]] && sudo apt-get install golang-go
+    [[ $answer =~ [Yy] ]] && sudo apt-get install npm
 fi
 
 gnome-terminal --tab -e "./bash_scripts/build_frontend.sh" --tab -e "./bash_scripts/build_backend.sh"
