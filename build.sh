9e0075e556219ed165d779e30d6bae0691fca6a8
Peeter Kokk <peeter.kokk@gmail.com>
2016-07-15 13:34:29 +0300

    add node-js to build.sh in order to use npm

--- build.sh
+++ build.sh
@@ -6,6 +6,13 @@ if [ $(dpkg-query -W -f='${Status}' golang-go 2>/dev/null | grep -c "ok installe
     [[ $answer =~ [Yy] ]] && sudo apt-get install golang-go
 fi
 
+
+if [ $(dpkg-query -W -f='${Status}' node-js 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+    read -p "node-js required but not installed. Install? [Y/n]: " answer
+    answer=${answer:=Y}
+    [[ $answer =~ [Yy] ]] && sudo apt-get install node-js
+fi
+
 if [ $(dpkg-query -W -f='${Status}' npm 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     read -p "npm required but not installed. Install? [Y/n]: " answer
     answer=${answer:=Y}
